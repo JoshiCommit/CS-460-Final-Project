11813c4e3ba38c9ec278840933c89134c3bed088
Deepanshu Joshi <deepanshu@Deepanshus-MacBook-Air.local>
2026-05-14 22:40:18 -0700

    Part 1-4 of coding completed

--- torchbearer.py
+++ torchbearer.py
@@ -25,77 +25,67 @@ import heapq
 # =============================================================================
 
 def explain_problem():
-    """
-    Returns
-    -------
-    str
-        Your Part 1 README answers, written as a string.
-        Must match what you wrote in README Part 1.
-
-    TODO
-    """
-    return "TODO"
-
+    return(
+        "The reason that a single shortest-path run from S is not sufficient is due to the fact that this will only give you the shortest path from the entrance to other nodes. This isn't all that the problem is asking however we also need to find what order the relic chambers need to be visited which a single shortest-path run from S will not be able to suffice. The structural decision that remains after the iner-locational travel costs are known is what order to visit the relic chambers in. Since relic order needs to be determined and this can make different total fuel costs we need a search over order to find a efficient visit order instead of a single computation giving shortest path from A - B."
+    )
 
 # =============================================================================
 # PART 2
 # =============================================================================
 
 def select_sources(spawn, relics, exit_node):
-    """
-    Parameters
-    ----------
-    spawn : node
-    relics : list[node]
-    exit_node : node
-
-    Returns
-    -------
-    list[node]
-        No duplicates. Order does not matter.
-
-    TODO
-    """
-    pass
+    # Combining spawn node with relic node but getting rid of duplicates
+    return list(dict.fromkeys([spawn] + relics))
 
 
 def run_dijkstra(graph, source):
-    """
-    Parameters
-    ----------
-    graph : dict[node, list[tuple[node, int]]]
-        graph[u] = [(v, cost), ...]. All costs are nonnegative integers.
-    source : node
+    # Storing nodes in graph
+    nodes = set(graph.keys())
 
-    Returns
-    -------
-    dict[node, float]
-        Minimum cost from source to every node in graph.
-        Unreachable nodes map to float('inf').
+    # Adding neighbor nodes that might not show up
+    for node in graph:
+        for neighbor, cost in graph[node]:
+            node.add(neighbor)
 
-    TODO
-    """
-    pass
+    # Put all distances to default value of infinity
+    distances = {}
+    for node in nodes:
+        distances[node] = float("inf")
+
+    # All distanses other than source unknown
+    distances[source] = 0
+
+    # Priority queue to store distance per node
+    distpq = []
+
+    heapq.heappush(distpq, (0, source))
+
+    while distpq:
+        # Node with smallest distance so far
+        curr_dist, curr_node = heapq.heappop(distpq)
+        # Making sure to skip and queue entries that are outdated
+        if curr_dist != distances[curr_node]:
+            continue
+        # Explore nodes connected to the current node
+        for neighbor, cost in graph.get(curr_node, []):
+            # Find the distance
+            new_dist = curr_dist + cost
+            # Check if new distance is shorter
+            if new_dist < distances[neighbor]:
+                # Set new distance as shortest distance
+                distances[neighbor] = new_dist
+                heapq.heappush(distpq, (new_dist, neighbor))
+    return distances
 
 
 def precompute_distances(graph, spawn, relics, exit_node):
-    """
-    Parameters
-    ----------
-    graph : dict[node, list[tuple[node, int]]]
-    spawn : node
-    relics : list[node]
-    exit_node : node
-
-    Returns
-    -------
-    dict[node, dict[node, float]]
-        Nested structure supporting dist_table[u][v] lookups
-        for every source u your design requires.
-
-    TODO
-    """
-    pass
+    # Get source nodes
+    source_nodes = select_sources(spawn, relics, exit_node)
+    dist_table = {}
+    # Running dijkstra
+    for source in source_nodes:
+        dist_table[source] = run_dijkstra(graph, source)
+    return dist_table
 
 
 # =============================================================================
@@ -103,16 +93,9 @@ def precompute_distances(graph, spawn, relics, exit_node):
 # =============================================================================
 
 def dijkstra_invariant_check():
-    """
-    Returns
-    -------
-    str
-        Your Part 3 README answers, written as a string.
-        Must match what you wrote in README Part 3.
-
-    TODO
-    """
-    return "TODO"
+    return(
+        "Current distance is for sure the shortest distance to the node meaning that their values are set in stone, Current distance is the shortest known distance using a path that goes through nodes who are already finalized. All nodes are undiscovered at this point meaning that their distances are set to infiity and only the starting point is known which is given to have a distance of 0 meaning that there is no incorrect distances. Since the algorithm will pick the node with the shortest distance and all edge weights are nonnegative meaning distance cannot possibly get shorter more efficient paths cannot be found to the node. By the end all nodes which are reachable will have the shortest distances mapped out and all nodes unreachable will have a distance of infinity. If the distances are off then there is a high chance that the algorithm might choose a route that isn't as optimal as it could be which leads to burning more fuel than needed."
+    )
 
 
 # =============================================================================
@@ -120,16 +103,9 @@ def dijkstra_invariant_check():
 # =============================================================================
 
 def explain_search():
-    """
-    Returns
-    -------
-    str
-        Your Part 4 README answers, written as a string.
-        Must match what you wrote in README Part 4.
-
-    TODO
-    """
-    return "TODO"
+    return(
+        "S connects to A with cost 1 and B with cost 2, A connects to B with cost 10 and T with cost 1, B connects to A with cost 1 and T with 1, Important to note there is no way to access anything from T. Greedy will pick S -> A -> B -> T. This results in a cost of 12 since there is no way for T to access B going T -> B. Optimal will pick S -> B -> A -> T. Because it is able to see the cost of 10 ahead it makes a more optimal choice having a cost of 4. While choosing the closest relic seems like a good idea it can lead to a path that is worse overall since it does not account for future costs and only looks at the next relic cost. The algorithm has to look at different visiting orders for relics to determine the order that uses the least total fuel "
+    )
 
 
 # =============================================================================
